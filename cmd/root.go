fc44dd6c89c5a2f9e8fc7308518e9f37ebdb937c
Jason <me@jasonuc.com>
2025-01-25 19:41:05 +0000

    feat: init using cobra cli

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"os"
+
+	cc "github.com/ivanpirog/coloredcobra"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gignr",
+	Short: "Effortlessly manage and generate .gitignore files",
+	Long: `gignr is a CLI tool designed to help you fetch, manage, and customize .gitignore templates 
+from popular repositories. Simplify your project setup with ease.
+
+Created by github.com/jasonuc.
+Visit https://github.com/jasonuc/gignr for more information.`,
+}
+
+func Execute() {
+	cc.Init(&cc.Config{
+		RootCmd:  rootCmd,
+		Headings: cc.HiCyan + cc.Bold + cc.Underline,
+		Commands: cc.HiYellow + cc.Bold,
+		Example:  cc.Italic,
+		ExecName: cc.Bold,
+		Flags:    cc.Bold,
+	})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gignr.yaml)")
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,14 @@ module github.com/jasonuc/gignr
 go 1.23.4
 
 require golang.org/x/mod v0.22.0
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/ivanpirog/coloredcobra v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
