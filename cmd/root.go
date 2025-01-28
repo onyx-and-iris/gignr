a89191446ce64a31c5a6464b2b52a2c0dff147bc
Jason <me@jasonuc.com>
2025-01-28 15:36:10 +0000

    refactor: remove unnecessary comments in root command initialization

--- cmd/root.go
+++ cmd/root.go
@@ -34,9 +34,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gignr.yaml)")
 }
