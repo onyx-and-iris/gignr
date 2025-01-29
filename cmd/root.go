437753363d8399d998313e96c17f7d68b8441c22
Jason <me@jasonuc.com>
2025-01-29 20:07:12 +0000

    feat: add configuration management using viper with default config creation

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"github.com/fatih/color"
 	cc "github.com/ivanpirog/coloredcobra"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -17,6 +18,10 @@ from popular repositories. Simplify your project setup with ease.
 
 Created by github.com/jasonuc.
 Visit %v for more information.`, color.New(color.FgBlue).Sprint("https://github.com/jasonuc/gignr")),
+	Run: func(cmd *cobra.Command, args []string) {
+		viper.Set("aaa", "vvv")
+		viper.WriteConfig()
+	},
 }
 
 func Execute() {
@@ -32,5 +37,38 @@ func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	configDir := home + "/.config/gignr"
+
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		cobra.CheckErr(err)
+	}
+
+	viper.AddConfigPath(configDir)
+
+	if err := viper.ReadInConfig(); err != nil {
+		// If the file does not exist, create a default one
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			configFile := configDir + "/config.yaml"
+			if err := viper.WriteConfigAs(configFile); err != nil {
+				cobra.CheckErr(err)
+			}
+
+			return
+		}
+		cobra.CheckErr(err)
 	}
 }
