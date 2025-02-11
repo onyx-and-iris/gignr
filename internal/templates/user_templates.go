4e75d2a0b78fd1d2e5187b7ed7af19bd6dbec670
Jason <me@jasonuc.com>
2025-02-11 23:41:36 +0000

    feat: Refactor user template fetching to load custom repositories from config

--- internal/templates/user_templates.go
+++ internal/templates/user_templates.go
@@ -1,27 +1,11 @@
 package templates
 
 import (
-	"fmt"
-
-	"github.com/jasonuc/gignr/internal/utils"
 	"github.com/spf13/viper"
 )
 
-func FetchUserTemplates() []Template {
-	userRepos := viper.GetStringMapString("repositories")
-	var userTemplates []Template
-
-	for nickname, repoURL := range userRepos {
-		owner, repo := utils.ExtractRepoDetails(repoURL)
-		repoTemplates, err := FetchTemplates(owner, repo, "")
-		if err != nil {
-			continue
-		}
-		for _, tmpl := range repoTemplates {
-			tmpl.Name = fmt.Sprintf("%s:%s", nickname, tmpl.Name)
-			userTemplates = append(userTemplates, tmpl)
-		}
-	}
-
-	return userTemplates
+// Load user-added repositories using urls from config
+func LoadCustomRepositories() map[string]string {
+	repos := viper.GetStringMapString("repositories")
+	return repos
 }
