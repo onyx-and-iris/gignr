9cd38c2d573323c4a0c1319a7601a38202b5a865
Jason <me@jasonuc.com>
2025-02-11 20:06:18 +0000

    feat: add utility functions for colored console output

---
+++ internal/utils/print.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func PrintError(err string) {
+	fmt.Println(color.New(color.BgRed, color.FgWhite).Sprint(" Error "), err)
+}
+
+func PrintWarning(warning string) {
+	fmt.Println(color.New(color.BgYellow, color.FgBlack).Sprint(" Warning "), warning)
+}
+
+func PrintSuccess(success string) {
+	fmt.Println(color.New(color.BgGreen, color.FgBlack).Sprint(" Success "), success)
+}
+
+func PrintAlert(alert string) {
+	fmt.Println(color.New(color.BgMagenta, color.FgWhite).Sprint(" Alert "), alert)
+}
